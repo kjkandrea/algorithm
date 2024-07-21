cc6c53971d66b39a77d621fb211596a6b1112455
kjkandrea <kjkandrea@gmail.com>
2024-07-21 15:16:27 +0900

    feat: 선택 정렬. python

---
+++ 02_sort/02-01_selection-sort.py
@@ -0,0 +1,14 @@
+def selection_sort(array, n):
+    for i in range(n - 1):
+        min = i;
+        for j in range(i + 1, n):
+            if (array[min] > array[j]):
+                min = j;
+        array[i], array[min] = array[min], array[i]
+    return array
+
+
+unsorted_array = [30, 20, 40, 35, 5, 10, 45, 50, 25, 15]
+sorted_array = selection_sort(unsorted_array, len(unsorted_array))
+
+print(sorted_array)
