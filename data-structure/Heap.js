ca4ad92d0a33aaf0cb9916d8bbf34f0169c5ffc5
kjkandrea <kjkandrea@gmail.com>
2024-08-05 12:11:28 +0900

    feat: heap.insert 구현

---
+++ data-structure/Heap.js
@@ -0,0 +1,30 @@
+export default class Heap {
+  heap = []
+
+  insert(value) {
+    this.heap.push(value)
+    this.heapifyUp()
+  }
+
+  heapifyUp() {
+    let index = this.heap.length - 1
+
+    while (index > 0) {
+      const parentIndex = this.getParentIndex(index)
+      if (this.heap[parentIndex] > this.heap[index]) {
+        this.swap(parentIndex, index)
+        index = parentIndex
+      } else {
+        break
+      }
+    }
+  }
+
+  getParentIndex(index) {
+    return Math.floor((index - 1) / 2)
+  }
+
+  swap(index1, index2) {
+    [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]]
+  }
+}
