571dd33e36837695ad741f5d593bcdcd6b1dac94
kjkandrea <kjkandrea@gmail.com>
2024-08-05 15:33:41 +0900

    refactor: 병합 정렬

--- 04_sort/04-02_merge-sort.js
+++ 04_sort/04-02_merge-sort.js
@@ -1,11 +1,14 @@
-function merge_sort(array, start = 0, end = array.length -1) {
+function mergeSort(array, start = 0, end = array.length -1) {
   if (start >= end) {
     return [array[start]];
   }
   const mid = Math.floor((start + end) / 2)
-  const left = merge_sort(array, start, mid)
-  const right = merge_sort(array, mid + 1, end)
 
+  // 절반을 나누어 각 부분 배열에 순환적으로 합병 정렬을 시킴
+  const left = mergeSort(array, start, mid)
+  const right = mergeSort(array, mid + 1, end)
+
+  // 정렬된 두 부분 배열을 합침
   return merge(left, right)
 }
 
@@ -14,6 +17,7 @@ function merge(left, right) {
   let leftIndex = 0
   let rightIndex = 0
 
+  // 작은 데이터를 왼 쪽에, 큰 데이터를 오른 쪽에 배치
   while(leftIndex < left.length && rightIndex < right.length) {
     if (left[leftIndex] < right[rightIndex]) {
       sorted_array.push(left[leftIndex])
@@ -38,6 +42,6 @@ function merge(left, right) {
 }
 
 const unsorted_array = [30, 20, 40, 35, 5, 10, 45, 50, 25, 15]
-const sorted_array = merge_sort(unsorted_array)
+const sorted_array = mergeSort(unsorted_array)
 
 console.log(sorted_array)
