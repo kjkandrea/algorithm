12c4fe3e02b1a2b497d014e98f60a1daa64cc79c
kjkandrea <kjkandrea@gmail.com>
2024-08-08 21:48:19 +0900

    feat: 인접 리스트 그래프 구현

---
+++ data-structure/Graph.js
@@ -0,0 +1,26 @@
+export default class Graph {
+  adjacencyList = {}
+
+  addVertex(vertex) {
+    if (this.adjacencyList[vertex]) return;
+    this.adjacencyList[vertex] = []
+  }
+
+  addEdge(vertex1, vertex2) {
+    if (!this.adjacencyList[vertex1]) {
+      this.addVertex(vertex1)
+    }
+
+    if (!this.adjacencyList[vertex2]) {
+      this.addVertex(vertex2)
+    }
+
+    if (!this.adjacencyList[vertex1].includes(vertex2)) {
+      this.adjacencyList[vertex1].push(vertex2)
+    }
+
+    if (!this.adjacencyList[vertex2].includes(vertex)) {
+      this.adjacencyList[vertex2].push(vertex1)
+    }
+  }
+}
