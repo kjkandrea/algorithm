801d6a4d04bb61243fc09c9bd4563528bfeb06f5
kjkandrea <kjkandrea@gmail.com>
2024-08-08 22:09:07 +0900

    feat: dfs 구현. 첫 정점 중복방문 버그 제거

--- 08_graph/08-01_depth-first-search.js
+++ 08_graph/08-01_depth-first-search.js
@@ -7,7 +7,10 @@ function depthFirstSearch(graph, startVertex) {
 
   const result = []
   const visitedTable = {}
+  visitedTable[startVertex] = true;
+
   let current;
+
   while (!stack.isEmpty()) {
     current = stack.pop()
     result.push(current)
@@ -24,13 +27,13 @@ function depthFirstSearch(graph, startVertex) {
 }
 
 const graph = new Graph()
-
-graph.addEdge(1, 2)
-graph.addEdge(1, 3)
-graph.addEdge(2, 5)
-graph.addEdge(3, 5)
-graph.addEdge(1, 4)
 graph.addEdge(4, 6)
+graph.addEdge(1, 4)
+graph.addEdge(3, 5)
+graph.addEdge(2, 5)
+graph.addEdge(1, 3)
+graph.addEdge(1, 2)
+
 graph.display()
 
 const result = depthFirstSearch(graph, 1)
